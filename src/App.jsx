7f80d5ecc8974e4fd93e50326d337fa9d75ebf1b
padmakumar18 <padmakumar41759@gmail.com>
2025-02-20 23:15:26 +0530

    Need to add Tailwind css

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Login from './Login';
 
 function App() {
   const [count, setCount] = useState(0)
@@ -9,7 +10,7 @@ function App() {
   return (
     <>
       <div className="container">
-        <p>Helloooo</p>
+        <Login/>
       </div>
     </>
   )
---
+++ tailwind.config.js
@@ -0,0 +1,7 @@
+export default {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"], // Scans all files inside src/
+  theme: {
+    extend: {}, // You can customize styles here later
+  },
+  plugins: [], // Add Tailwind plugins here if needed
+};
